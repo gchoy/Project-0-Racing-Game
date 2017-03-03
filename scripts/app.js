3c84ba59b2c89572cea2afe56cb50647ad8c3e77
gchoy <gabriela.choy@gmail.com>
2017-03-03 15:58:46 -0800

    added car control upon keydown keyup

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-$(document).on("ready", function() {
+//$(document).on("ready", function() {
   //e.preventDefault();
   //$("#audi").hide();
   //$("#audi").slideDown(1000);
@@ -6,8 +6,40 @@ $(document).on("ready", function() {
   //$("#audi").show();
   //$("#audi").slideDown(3000);
 
-  
+  //on click
+  //moveRight();
+$(document).ready(function() {
+    // $("#viper").animate({left: "+=500"}, 2000);
+     //$("#viper").animate({left: "-=300"}, 1000);
 
 
 
+setInterval(moveCar, 20);
+var keys = {}
+
+$(document).keydown(function(e) {
+    keys[e.keyCode] = true;
+});
+
+$(document).keyup(function(e) {
+    delete keys[e.keyCode];
+});
+
+function moveCar() {
+    for (var direction in keys) {
+        if (!keys.hasOwnProperty(direction)) continue;
+        if (direction == 37) {
+            $("#viper").animate({left: "-=5"}, 0);
+        }
+        if (direction == 38) {
+            $("#viper").animate({top: "-=5"}, 0);
+        }
+        if (direction == 39) {
+            $("#viper").animate({left: "+=5"}, 0);
+        }
+        if (direction == 40) {
+            $("#viper").animate({top: "+=5"}, 0);
+        }
+    }
+}
 });
