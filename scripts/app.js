24ec9e53c3b138772785156c83b6e35fac714e22
gchoy <gabriela.choy@gmail.com>
2017-03-03 14:44:02 -0800

    Created a board for each player

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,12 @@
 $(document).on("ready", function() {
+  //e.preventDefault();
+  //$("#audi").hide();
+  //$("#audi").slideDown(1000);
+  //$("#audi").hide();
+  //$("#audi").show();
+  //$("#audi").slideDown(3000);
 
   
-
 
 
 
