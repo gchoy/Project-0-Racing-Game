56019f73f161635c76f916d1a111f7c8970d03f2
gchoy <gabriela.choy@gmail.com>
2017-03-04 18:34:26 -0800

    Added winning condition to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
 
 setInterval(moveCar, 20);
+
+
 var keys = {}
 
 $(document).keydown(function(e) {
@@ -14,16 +16,39 @@ $(document).keyup(function(e) {
 });
 
 function moveCar() {
+
+
     for (var direction in keys) {
+        var player1 = 0;
+        var player2 = 0;
 
         if (direction == 68) {
-            $("#audi").animate({left: "+=5"}, 0);
+            $("#redcar").animate({left: "+=5"}, 0);
+            player1 += 1;
+
         }
 
         if (direction == 39) {
-            $("#viper").animate({left: "+=5"}, 0);
+            $("#bluecar").animate({left: "+=5"}, 0);
+            player2 += 1;
+
         }
+
+        if ($('#redcar').offset().left >=1080 && $('#redcar').offset().left>$('#bluecar').offset().left){
+          console.log("Player 1 winner");
+          $("#redcar").animate({left: "-=10"}, 0);
+          alert("Player1 is the winner");
+        }
+        if ($('#bluecar').offset().left >=1080 && $('#bluecar').offset().left>$('#redcar').offset().left){
+          console.log("Player 2 winner");
+          $("#bluecar").animate({left: "-=10"}, 0);
+          alert("Player2 is the winner");
+        }
+
 
     }
 }
+
+
+
 });
