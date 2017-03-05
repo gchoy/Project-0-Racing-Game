82836100ee0bebfd257ef5175c41f71202308aa6
gchoy <gabriela.choy@gmail.com>
2017-03-04 22:36:08 -0800

    writting winner to the console log

--- scripts/app.js
+++ scripts/app.js
@@ -19,31 +19,30 @@ function moveCar() {
 
 
     for (var direction in keys) {
-        var player1 = 0;
-        var player2 = 0;
+        var winner = "";
 
         if (direction == 68) {
             $("#redcar").animate({left: "+=5"}, 0);
-            player1 += 1;
+
 
         }
 
         if (direction == 39) {
             $("#bluecar").animate({left: "+=5"}, 0);
-            player2 += 1;
+
 
         }
 
         if ($('#redcar').offset().left >=1080 && $('#redcar').offset().left>$('#bluecar').offset().left){
           console.log("Player 1 winner");
-          $("#redcar").animate({left: "-=10"}, 0);
-          alert("Player1 is the winner");
-        }
-        if ($('#bluecar').offset().left >=1080 && $('#bluecar').offset().left>$('#redcar').offset().left){
+          $("#redcar").animate({left: "-=10"}, 10000);
+          winner = "Player 1";
+        } else if ($('#bluecar').offset().left >=1080 && $('#bluecar').offset().left>$('#redcar').offset().left){
           console.log("Player 2 winner");
-          $("#bluecar").animate({left: "-=10"}, 0);
-          alert("Player2 is the winner");
+          $("#bluecar").animate({left: "-=10"}, 10000);
+          winner = "Player 2";
         }
+
 
 
     }
